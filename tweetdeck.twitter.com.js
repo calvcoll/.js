fe037afc9e3c51563353a91cb0448cd6c5bd3eeb
Calv Collins <calvcoll@gmail.com>
2014-09-06 00:52:51 +0100

    Fixed tweetdeck hating.

--- tweetdeck.twitter.com.js
+++ tweetdeck.twitter.com.js
@@ -1,5 +1,15 @@
 var documentReady = function() {
-	console.log("dotjs ran")
+	console.log("dotjs started");
+	/* type is either "light" or "dark" */
+	var type = "light";
+	var style = "";
+	$.get("https://github.com/pixeldesu/cashmere/releases/download/v1.1.0/cashmere-" + type + ".css", function (data) {
+		style = data;
+	}).done(function () {
+		styletag = $(document).add("style").html(style);
+	}).error(function () {
+		alert("something went very, very wrong");
+	});
 }
 
 $(document).ready(documentReady)
